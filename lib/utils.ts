61b8e8d668def8a5fb405cbc66841f4cf528913f
unknown <kunjshah345.ks@gmail.com>
2025-11-30 16:48:15 +0530

    Next js Task management

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-import { IRoute } from '@/types/navigation';
 import Router from 'next/router';
 
 export function truncate(str: string, length: number) {
@@ -14,42 +13,6 @@ export function truncate(str: string, length: number) {
 
 // NextJS Requirement
 export const isWindowAvailable = () => typeof window !== 'undefined';
-
-export const findCurrentRoute = (
-  routes: IRoute[],
-  pathname: string,
-): IRoute | null | undefined => {
-  if (!isWindowAvailable()) return null;
-
-  for (let route of routes) {
-    if (!!route.items) {
-      const found = findCurrentRoute(route.items, pathname);
-      if (!!found) return found;
-    }
-    // console.log("route", route)
-    if (pathname?.match(route.path) && route) return route;
-  }
-};
-
-export const getActiveRoute = (routes: IRoute[], pathname: string): string => {
-  const route = findCurrentRoute(routes, pathname);
-  return route?.name || "Tasks";
-};
-
-export const getActiveNavbar = (
-  routes: IRoute[],
-  pathname: string,
-): boolean | undefined => {
-  const route = findCurrentRoute(routes, pathname);
-  return route?.secondary;
-};
-
-export const getActiveNavbarText = (
-  routes: IRoute[],
-  pathname: string,
-): string | boolean => {
-  return getActiveRoute(routes, pathname) || false;
-};
 
 export function firstLetterCapital(letter: string) {
   if (letter != undefined && letter != null && letter != "") {
@@ -87,9 +50,9 @@ export const generateValue = (value: number, decimals?: number) => {
   if (isNaN(value)) {
     return "";
   }
-  if(decimals){
+  if (decimals) {
     return Number(value).toFixed(decimals);
-  }else{
+  } else {
     return Number(value);
   }
 };
