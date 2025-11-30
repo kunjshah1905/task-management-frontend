0f180a9c6bed85befc17f4363af5926cd7750d7a
unknown <kunjshah345.ks@gmail.com>
2025-12-01 00:43:17 +0530

    Next js Task management

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_APP_API_URL || 'http://localhost:8000/api',
     },
     images: {
         remotePatterns: [
